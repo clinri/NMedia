2d095e72558743c3e9ff034cb3fb66c989471242
Andrei Borisov <clinri@ya.ru>
2022-08-18 16:39:48 +0300

    Done homework "14_pushes" task "Exceptions": version 1

---
+++ app/src/main/java/ru/netology/nmedia/service/Action.kt
@@ -0,0 +1,13 @@
+package ru.netology.nmedia.service
+
+internal enum class Action(
+    val key: String
+) {
+    Like("LIKE");
+
+    companion object{
+        const val KEY = "action"
+    }
+
+
+}
